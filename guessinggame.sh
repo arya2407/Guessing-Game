f6cc029671aad43cd260af215b00bf2907d27f73
Arya Ravi <arya.ravi.2407@gmail.com>
2021-09-12 20:17:14 +0800

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "-----WELCOME TO THE GUESSING GAME-----"
+
+echo "The goal of this game is to try and correctly guess the number of files in the currenct directory"
+
+function guess(){
+    true_ans=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "please guess how many files are in the current directory, and type your answer here: "
+        read  number
+        if [ $number -lt $true_ans ]
+        then
+            echo "Sorry. Your guess was too small - there are more files than that in this directory"
+        elif [ $number -gt $true_ans ]
+        then
+            echo "Sorry. Your guess was too large - there are not that many files in this directory"
+        else
+            echo " Congratulations! You correctly guessed the number of files in this directory - well done!"
+        break;
+        fi
+    done
+}
+
+guess
